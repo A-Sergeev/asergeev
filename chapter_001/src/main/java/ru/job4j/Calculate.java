f332a97f8f846f6cb1cec47c55be665b103143ec
A-Sergeev <vifsergeev@gmail.com>
2018-01-25 19:18:41 +0300

    1.3 JavaDoc #4086

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,5 +1,12 @@
 package ru.job4j;
 
+/**
+ * Package for calculate task.
+ * @author asergeev
+ * @since 25.01.2018
+ * @version 1
+ */
+
 public class Calculate {
 	public static void main(String[] args){
 		System.out.println("Hello world");
---
+++ chapter_001/src/main/java/ru/job4j/PackageInfo.java
@@ -0,0 +1,9 @@
+/**
+ * Package for calculate task.
+ *
+ * @author asergeev (vifsergeev@gmail.com)
+ * @since 25.01.2018
+ * @version 1
+ */
+
+package ru.job4j;
